e7b8209953fcd6c448fbb389a27485a1a5e09e31
Moritz Heiber <hello@heiber.im>
2023-01-04 12:02:51 +0100

    Finished version logic, renamed Version to Release, added semver

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,64 @@
+use std::cmp::Ordering;
 use std::error::Error;
+use std::ops::Range;
 use std::process;
 
 use csv::ReaderBuilder;
 use regex::Regex;
-use serde::{Deserialize, Serialize};
+use semver::Version as SemVerVersion;
+use serde::de::Error as SerdeError;
+use serde::{Deserialize, Deserializer, Serialize};
 
 const RELEASE_URL: &str = "https://cache.ruby-lang.org/pub/ruby/index.txt";
+const VERSION_RANGE: Range<u64> = 0..99;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Version {
-    name: String,
+#[derive(Debug, Serialize, Deserialize, Eq, Clone)]
+struct Release {
+    #[serde(rename = "name")]
+    #[serde(deserialize_with = "parse_semver_version")]
+    version: SemVerVersion,
     url: String,
     sha256: String,
 }
 
-impl Version {
-    fn valid(&self) -> bool {
-        is_regular_release(&self.name) && has_tar_gz_url(&self.url)
+fn parse_semver_version<'de, D>(deserializer: D) -> Result<SemVerVersion, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let version: String = String::deserialize(deserializer)?;
+    let version = version.strip_prefix("ruby-").unwrap();
+    version.parse().map_err(D::Error::custom)
+}
+
+impl Ord for Release {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.version.cmp(&other.version)
     }
 }
 
-fn is_regular_release(r: &str) -> bool {
-    let regex = Regex::new(r"^ruby-3\.\d{1,2}\.\d{1,2}$").unwrap();
-    regex.is_match(r)
+impl PartialOrd for Release {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Release {
+    fn eq(&self, other: &Self) -> bool {
+        self.version == other.version
+    }
+}
+
+impl Release {
+    fn valid(&self) -> bool {
+        is_regular_release(&self.version) && has_tar_gz_url(&self.url)
+    }
+}
+
+fn is_regular_release(r: &SemVerVersion) -> bool {
+    r.major == 3
+        && VERSION_RANGE.contains(&r.minor)
+        && VERSION_RANGE.contains(&r.patch)
+        && r.pre.is_empty()
 }
 
 fn has_tar_gz_url(u: &str) -> bool {
@@ -30,14 +66,17 @@ fn has_tar_gz_url(u: &str) -> bool {
     regex.is_match(u)
 }
 
-async fn parse_data<'a>(csv: &'a str) -> Result<Vec<Version>, Box<dyn Error>> {
+async fn parse_data<'a>(csv: &'a str) -> Result<Vec<Release>, Box<dyn Error>> {
     let mut result = vec![];
     let mut csv = ReaderBuilder::new()
         .delimiter(b'\t')
         .from_reader(csv.as_bytes());
 
     for line in csv.deserialize() {
-        let item: Version = line?;
+        let item: Release = match line {
+            Ok(release) => release,
+            Err(_) => continue,
+        };
         if item.valid() {
             result.push(item)
         }
@@ -45,8 +84,18 @@ async fn parse_data<'a>(csv: &'a str) -> Result<Vec<Version>, Box<dyn Error>> {
     Ok(result)
 }
 
-async fn latest_versions(versions: Vec<Version>) -> Vec<Version> {
-    vec![]
+async fn latest_versions(versions: Vec<Release>) -> Vec<Release> {
+    let mut releases: Vec<Release> = vec![];
+    for number in VERSION_RANGE {
+        let mut v = versions.clone();
+        v.retain(|r| r.version.minor == number);
+        v.sort();
+        if let Some(r) = v.last() {
+            releases.push(r.to_owned())
+        }
+    }
+
+    releases
 }
 
 #[tokio::main]
@@ -59,38 +108,23 @@ async fn main() {
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    use rand::prelude::*;
     use std::fs;
 
-    use crate::{has_tar_gz_url, is_regular_release, latest_versions, parse_data, Version};
-    use csv::ReaderBuilder;
-    use rand::prelude::*;
-
-    struct Release {
-        name: &'static str,
+    struct Data {
+        version: &'static str,
         url: &'static str,
     }
 
     #[test]
-    fn parses_regular_release() {
-        let good = good_releases();
-        let bad = bad_releases();
-
-        for release in good {
-            assert!(is_regular_release(release.name))
-        }
-
-        for release in bad {
-            assert!(!is_regular_release(release.name))
-        }
-    }
-
-    #[test]
     fn validates_good_version() {
-        for version in convert_to_versions(good_releases()) {
+        for version in convert_to_versions(good_data()) {
             assert!(version.valid())
         }
 
-        for version in convert_to_versions(good_and_bad_releases_with_bad_urls()) {
+        for version in convert_to_versions(good_and_bad_data_with_bad_urls()) {
             assert!(!version.valid())
         }
     }
@@ -107,20 +141,27 @@ mod test {
     #[tokio::test]
     async fn parse_correct_csv() {
         let content = fs::read_to_string("test/fixtures/index.txt").unwrap();
-        let versions = parse_data(&content).await.unwrap();
-        let first: &Version = versions.first().unwrap();
-        assert_eq!(first.name, "ruby-3.0.0".to_string())
-    }
+        let releases = parse_data(&content).await.unwrap();
+        let first: &Release = releases.first().unwrap();
+        assert_eq!(
+            first.version,
+            SemVerVersion {
+                major: 3,
+                minor: 0,
+                patch: 0,
+                pre: semver::Prerelease::new("").unwrap(),
+                build: semver::BuildMetadata::EMPTY,
+            }
+        );
 
-    #[test]
-    fn parse_fixture() {
-        let mut csv = ReaderBuilder::new()
-            .delimiter(b'\t')
-            .from_path("test/fixtures/index.txt")
-            .unwrap();
-        for result in csv.deserialize() {
-            let _: Version = result.unwrap();
-        }
+        let latest = latest_versions(releases).await;
+        assert_eq!(latest.len(), 3);
+        assert_eq!(latest[0].version.minor, 0);
+        assert_eq!(latest[1].version.minor, 1);
+        assert_eq!(latest[2].version.minor, 2);
+        assert_eq!(latest[0].version.patch, 5);
+        assert_eq!(latest[1].version.patch, 3);
+        assert_eq!(latest[2].version.patch, 0);
     }
 
     #[tokio::test]
@@ -129,68 +170,83 @@ mod test {
 name	url	sha1	sha256	sha512
 ruby-3.1.1	https://cache.ruby-lang.org/pub/ruby/3.1/ruby-3.1.1.tar.gz	289cbb9eae338bdaf99e376ac511236e39be83a3	fe6e4782de97443978ddba8ba4be38d222aa24dc3e3f02a6a8e7701c0eeb619d	a60d69d35d6d4ad8926b324a6092f962510183d9759b096ba4ce9db2e254e0f436030c2a62741352efe72aec5ca2329b45edd85cca8ad3254a9c57e3d8f66319
 ";
-        let versions = parse_data(line).await.unwrap();
-        let version = versions.first().unwrap();
-        assert_eq!(version.name, "ruby-3.1.1")
+        let releases = parse_data(line).await.unwrap();
+        let release = releases.first().unwrap();
+        assert_eq!(
+            release.version,
+            SemVerVersion {
+                major: 3,
+                minor: 1,
+                patch: 1,
+                pre: semver::Prerelease::default(),
+                build: semver::BuildMetadata::EMPTY,
+            }
+        )
     }
 
     #[tokio::test]
     async fn returns_latest_versions() {
-        let versions = convert_to_versions(good_releases());
-        let latest = latest_versions(versions).await;
-        assert_eq!(latest.len(), 3)
+        let releases = convert_to_versions(good_data());
+        let latest = latest_versions(releases).await;
+        assert_eq!(latest.len(), 3);
+        assert_eq!(latest[0].version.minor, 0);
+        assert_eq!(latest[1].version.minor, 1);
+        assert_eq!(latest[2].version.minor, 2);
+        assert_eq!(latest[0].version.patch, 16);
+        assert_eq!(latest[1].version.patch, 12);
+        assert_eq!(latest[2].version.patch, 11);
     }
 
-    fn convert_to_versions(releases: Vec<Release>) -> Vec<Version> {
-        let mut versions = vec![];
-        for release in releases {
-            versions.push(Version {
-                name: release.name.to_owned(),
-                url: release.url.to_owned(),
+    fn convert_to_versions(data: Vec<Data>) -> Vec<Release> {
+        let mut releases = vec![];
+        for item in data {
+            releases.push(Release {
+                version: item.version.parse::<SemVerVersion>().unwrap(),
+                url: item.url.to_owned(),
                 sha256: "sha256".to_string(),
             })
         }
-        versions
+        releases
     }
 
-    fn good_releases() -> Vec<Release> {
+    fn good_data() -> Vec<Data> {
         let mut releases = vec![];
-        for (name, url) in &[
-            ("ruby-3.2.0", good_url()),
-            ("ruby-3.2.11", good_url()),
-            ("ruby-3.2.2", good_url()),
-            ("ruby-3.1.0", good_url()),
-            ("ruby-3.1.12", good_url()),
-            ("ruby-3.0.5", good_url()),
-            ("ruby-3.0.16", good_url()),
+        for (version, url) in &[
+            ("3.2.0", good_url()),
+            ("3.2.11", good_url()),
+            ("3.2.2", good_url()),
+            ("3.1.0", good_url()),
+            ("3.1.12", good_url()),
+            ("3.0.5", good_url()),
+            ("3.0.16", good_url()),
         ] {
-            releases.push(Release { name, url })
+            releases.push(Data { version, url })
         }
 
         releases
     }
 
-    fn bad_releases() -> Vec<Release> {
-        let mut releases = vec![];
-        for (name, url) in &[
-            ("ruby-2.7.0", one_bad_url()),
-            ("ruby-3.2.0-preview1", one_bad_url()),
-            ("ruby-3.2.0-rc2", one_bad_url()),
-            ("ruby-3.1.5-something", one_bad_url()),
+    fn bad_data() -> Vec<Data> {
+        let mut data = vec![];
+        for (version, url) in &[
+            ("2.7.0", one_bad_url()),
+            ("3.2.0-preview1", one_bad_url()),
+            ("3.2.0-rc2", one_bad_url()),
+            ("3.1.5-something", one_bad_url()),
         ] {
-            releases.push(Release { name, url })
+            data.push(Data { version, url })
         }
-        releases
+        data
     }
 
-    fn good_and_bad_releases_with_bad_urls() -> Vec<Release> {
-        let mut releases = bad_releases();
-        releases.push(Release {
-            name: "3.2.0",
+    fn good_and_bad_data_with_bad_urls() -> Vec<Data> {
+        let mut data = bad_data();
+        data.push(Data {
+            version: "3.2.0",
             url: one_bad_url(),
         });
 
-        releases
+        data
     }
 
     fn good_url() -> &'static str {
